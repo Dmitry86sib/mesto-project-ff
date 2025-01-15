d3e83187565d4a5f46c6c4ae929807786b2c4758
Dmitry Granitov <duhsibiri@mail.ru>
2025-01-15 20:31:46 +0300

    написал первый скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const templateCard = document.querySelector('#card-template').content.querySelector('.card');
+const placesList = document.querySelector('.places__list');
+
+function handleDelete(card) {
+  card.remove();
+}
+
+function createCard(dataCard, { onDelete }) {
+  const newCard = templateCard.cloneNode(true);
+
+  newCard.querySelector('.card__title').textContent = dataCard.name;
+  newCard.querySelector('.card__image').src = dataCard.link;
+  newCard.querySelector('.card__image').alt = dataCard.name;
+
+  newCard.querySelector('.card__delete-button').addEventListener('click', ()=> {
+    onDelete(newCard);
+  })
+
+  return newCard;
+}
+
+initialCards.forEach(elem => placesList.append(createCard(elem, { onDelete: handleDelete })));
